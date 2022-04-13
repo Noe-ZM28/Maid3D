8e0023641d3eddcfbbef4ca8e9e2a49f618b6791
Noé Zúñiga <brinknene@gmail.com>
2022-04-13 18:50:21 -0500

    Se agregarn funciones para abrir archivos y escribir notas

--- Maid.py
+++ Maid.py
@@ -1,10 +1,12 @@
-from traceback import print_tb
 import pyttsx3
 import pywhatkit
 import speech_recognition as sr
 import wikipedia
 import datetime
 import keyboard
+import subprocess as subp
+import os
+import json
 
 name = "computadora" 
 listener = sr.Recognizer()
@@ -12,23 +14,38 @@ engine = pyttsx3.init()
 KEY = None
 
 voices = engine.getProperty('voices')
-#ver voces instaladas
-#for voice in voices:
-#    print(voice) 
+
+with open('info.json') as f:
+    data = json.load(f)
+
+sites = {
+    'google': 'google.com',
+    'youtube': 'youtube.com',
+    'escuela': 'http://cursos2.tlalnepantla.tecnm.mx',
+    'twitter': 'https://twitter.com/?lang=es'
+}
+files = {
+    'google': 'google.com',
+    'youtube': 'youtube.com',
+    'escuela': 'http://cursos2.tlalnepantla.tecnm.mx',
+    'twitter': 'https://twitter.com/?lang=es'
+}
+programs = {
+        "navegador": "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+        "escritorio remoto": "C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
+}
 
 engine.setProperty('voice', voices[1].id) # 1 para español o 0 para ingles
-
 engine.setProperty('rate', 178)
 engine.setProperty('volume', 0.7)
 
 def talk (text):
    engine.say(text)
    engine.runAndWait()
-#
 
-def listen():
+def listen(Something = None):
     try:
-        print('Escuchando...')
+        print(f'{Something}')
         with sr.Microphone() as source:
             voice = listener.listen(source)
             listener.adjust_for_ambient_noise(source, 0.5)
@@ -37,44 +54,97 @@ def listen():
                                             language='es-MX')
             rec = rec.lower()
         
-
     except sr.UnknownValueError:
-        pass#talk("Disculpa, no te he entendido, ¿me lo puedes repetir?")
+        pass
+        #talk("Disculpa, no te he entendido, ¿me lo puedes repetir?")
+    except wikipedia.PageError:
+        talk("Disculpa, no te he entendido, ¿me lo puedes repetir?")
+        pass
     except:
         pass
     return rec
 
+def write(file):
+    talk('¿Que necesitas que escriba?')
+    rec_write = listen("Escribiendo>: ")
+    print(f"{rec_write}")  
+
+    file.write(rec_write + os.linesep)
+    file.close()
+    talk('Terminé de escribir, este es el resultado')
+    subp.Popen("notas.txt", shell=True)
+
 def runVA():
     while True:
         try:
-            rec = listen()   
+            rec = listen("Esperando instrucción")   
             print(f"Escuchando>: {rec}")  
             if name in rec:
                 rec = rec.replace(name, '')
+
+
                 if 'reproduce' in rec:
                     music = rec.replace('reproduce','')
                     pywhatkit.playonyt(music)
                     print(f"Reproduciendo>: {music}")
                     talk(f"Reproduciendo {music}")
+
+
                 elif 'repite' in rec:
                     repeat = rec.replace('repite','')
                     talk(repeat)
-                elif "busca":
+
+
+                elif 'busca' in rec:
                     search = rec.replace('busca', '')
                     wikipedia.set_lang('es')
                     wiki = wikipedia.summary(search, 1)
                     talk(f'Buscando...{search}')
                     print(f"{search}>: {wiki}")
                     talk(wiki)
-                # else:
-                #     talk("Comando desconocido")
-            elif "termina":
+
+
+                elif 'abre' in rec:
+                    any = rec.replace('abre', '')
+                    for any in sites:
+                        if any in rec:
+                            talk(f"abriendo {any}")
+                            subp.call(f'start msedge.exe {sites[any]}', shell=True)
+                    for any in programs:
+                        if any in rec:
+                            talk(f"abriendo {any}")
+                            os.startfile({programs[any]})
+
+
+                elif 'archivo' in rec:
+                    file = rec.replace('abre', '')
+                    for file in files:
+                        if site in rec:
+                            talk(f"abriendo {file}")
+                            subp.Popen(f'{files[file]}', shell=True)
+
+
+                elif 'escribe' in rec:
+                    writte = rec.replace('escribe', '')
+                    try:
+                        with open("notas.txt", "a") as file:
+                            write(file)
+                    except FileNotFoundError as e:
+                        file = open("notas.txt", "w")
+                        write(file)
+
+
+                elif "termina" in rec:
+                    talk("Hasta pronto")
+                    break
+            elif "termina" in rec:
                 talk("Hasta pronto")
                 break
 
-    
+
         except UnboundLocalError:
-            #talk("No te entendí, intenta de nuevo...")
+            continue
+        except:
             continue
 
 
