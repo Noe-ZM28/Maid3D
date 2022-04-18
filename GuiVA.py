0844fdda68cde88d262b0b2d42925f031b88f15b
Noé Zúñiga <brinknene@gmail.com>
2022-04-17 20:43:49 -0500

    Se agrego imagen a antalla de gui

--- GuiVA.py
+++ GuiVA.py
@@ -1,164 +1,44 @@
-import pyttsx3
-import pywhatkit
-import speech_recognition as sr
-
-import wikipedia
-
-import subprocess as subp
-import os
-import json
-
 from tkinter import *
-from pygame import mixer
-from PIL import Image, ImageTk
+from PIL import ImageTk, Image
+import pyautogui
+import time
+
+
+width, height= pyautogui.size()
+pos_x = int(width/3)
+pos_y = int(height/8)
+width_screen = 600
+height_screen = 400
 
 main_window = Tk()
-main_window.title("Asistente")
-main_window.geometry("400x400")
-main_window.resizable(0, 0)
-main_window.configure(background="#BEABF9")
+main_window.geometry(f"{width_screen}x{height_screen}+{pos_x}+{pos_y}")
+#main_window.resizable(0, 0)
+main_window.configure(background = 'black')
+
+path_image = 'C:/Users/brink/Downloads/#Z/workspace/Maid3D/nezuko.jpg'
+path_image2 = 'C:/Users/brink/Downloads/#Z/workspace/Maid3D/animes.jpg'
+
+frame = Frame(main_window)
+frame.pack()
+frame.place(anchor='center', relx=0.5, rely=0.5)
+# Create an object of tkinter ImageTk
+img = ImageTk.PhotoImage(Image.open(path_image))
+
+# # Create a Label Widget to display the text or Image
+label = Label(frame, image = img)
+label.pack()
 
 
-name = "computadora" 
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-KEY = None
-
-voices = engine.getProperty('voices')
 
 
-sites = {
-    'google': 'google.com',
-    'youtube': 'youtube.com',
-    'escuela': 'http://cursos2.tlalnepantla.tecnm.mx',
-    'twitter': 'https://twitter.com/?lang=es'
-}
-files = {
-    'notas': 'notas.txt'
-}
-programs = {
-        "navegador": "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
-        "escritorio remoto": "C:\Program Files (x86)\TeamViewer\TeamViewer.exe",
-        "lol": "C:\Riot Games\Riot Client\RiotClientServices.exe",
-        "configuracion": "C:\Windows\System32\control.exe",
-        "word": "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE",
-        "excel": "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE",
-        "power point": "C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE",
-        "": "",
-}
-
-engine.setProperty('voice', voices[1].id) # 1 para español o 0 para ingles
-engine.setProperty('rate', 178)
-engine.setProperty('volume', 0.7)
-
-def talk (text):
-   engine.say(text)
-   engine.runAndWait()
-
-def listen(Something = None):
-    try:
-        print(f'{Something}')
-        with sr.Microphone() as source:
-            voice = listener.listen(source)
-            listener.adjust_for_ambient_noise(source, 0.5)
-            rec = listener.recognize_google(voice,
-                                            key=KEY,
-                                            language='es-MX')
-            rec = rec.lower()
-        
-    except sr.UnknownValueError:
-        pass
-        #talk("Disculpa, no te he entendido, ¿me lo puedes repetir?")
-    except wikipedia.PageError:
-        talk("Disculpa, no te he entendido, ¿me lo puedes repetir?")
-        pass
-    except:
-        pass
-    return rec
-
-def write(file):
-    talk('¿Que necesitas que escriba?')
-    rec_write = listen("Escribiendo>: ")
-    print(f"{rec_write}")  
-
-    file.write(rec_write + os.linesep)
-    file.close()
-    talk('Terminé de escribir, este es el resultado')
-    subp.Popen("notas.txt", shell=True)
-
-def runVA():
-    while True:
-        try:
-            rec = listen("Esperando instrucción")   
-            print(f"Escuchando>: {rec}")  
-            if name in rec:
-                rec = rec.replace(name, '')
 
 
-                if 'reproduce' in rec:
-                    music = rec.replace('reproduce','')
-                    pywhatkit.playonyt(music)
-                    print(f"Reproduciendo>: {music}")
-                    talk(f"Reproduciendo {music}")
+# def change_image_frame(image):
+#     # Create a Label Widget to display the text or Image
+#     img = ImageTk.PhotoImage(Image.open(image))
+#     label = Label(frame, image = img)
+#     label.pack()
 
-
-                elif 'repite' in rec:
-                    repeat = rec.replace('repite','')
-                    talk(repeat)
-
-
-                elif 'busca' in rec:
-                    search = rec.replace('busca', '')
-                    wikipedia.set_lang('es')
-                    wiki = wikipedia.summary(search, 1)
-                    talk(f'Buscando...{search}')
-                    print(f"{search}>: {wiki}")
-                    talk(wiki)
-
-
-                elif 'abre' in rec:
-                    any = rec.replace('abre', '')
-                    for any in sites:
-                        if any in rec:
-                            talk(f"abriendo {any}")
-                            subp.call(f'start msedge.exe {sites[any]}', shell=True)
-                    for any in programs:
-                        if any in rec:
-                            talk(f"abriendo {any}")
-                            subp.Popen({programs[any]})
-
-
-                elif 'archivo' in rec:
-                    file = rec.replace('abre', '')
-                    for file in files:
-                        if file in rec:
-                            talk(f"abriendo {file}")
-                            subp.Popen(f'{files[file]}', shell=True)
-
-
-                elif 'escribe' in rec:
-                    writte = rec.replace('escribe', '')
-                    try:
-                        with open("notas.txt", "a") as file:
-                            write(file)
-                    except FileNotFoundError as e:
-                        file = open("notas.txt", "w")
-                        write(file)
-
-
-                elif "termina" in rec:
-                    talk("Hasta pronto")
-                    break
-            elif "termina" in rec:
-                talk("Hasta pronto")
-                break
-
-
-        except UnboundLocalError:
-            continue
-        except:
-            continue
-
-
+# change_image_frame(path_image2)
 main_window.mainloop()
-runVA()
+
